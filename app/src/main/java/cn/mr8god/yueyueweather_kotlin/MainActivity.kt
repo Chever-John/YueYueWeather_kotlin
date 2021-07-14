abfc9a97d38d0a5a966cfd7a5c760a032849e1c8
Mr8god <Mr8god@hotmail.com>
2021-07-14 14:14:17 +0800

    Initial commit

---
+++ app/src/main/java/cn/mr8god/yueyueweather_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.mr8god.yueyueweather_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
